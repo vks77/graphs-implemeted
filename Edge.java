878124f318e355679d44c34ac7dc958845c358fe
Vinay Sharma <sharma.vinay1206@gmail.com>
2025-01-23 23:42:23 +0530

    added Vertex and Edge with Graph logic

--- Edge.java
+++ Edge.java
@@ -1,2 +1,25 @@
 public class Edge {
+
+    private final Vertex start;
+    private final Vertex end;
+    private final Integer weight;
+
+    public Edge (Vertex startV, Vertex endV, Integer inputWeight) {
+        this.start = startV;
+        this.end = endV;
+        this.weight = inputWeight;
+    }
+
+    public Vertex getStart() {
+        return this.start;
+    }
+
+    public Vertex getEnd() {
+        return this.end;
+    }
+
+    public Integer getWeight() {
+        return this.weight;
+    }
 }
+
